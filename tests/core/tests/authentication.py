0cf8f02e11c0c61101ec677102803c5e5238d49d
Matt Croydon <matt@lawrence.com>
2010-04-04 01:56:34 -0500

    Updated authentication tests to check for new HttpUnauthorized behavior and send correct headers.

--- tests/core/tests/authentication.py
+++ tests/core/tests/authentication.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.http import HttpRequest
 from django.test import TestCase
 from tastypie.authentication import Authentication, BasicAuthentication
-
+from tastypie.http import HttpUnauthorized
 
 class AuthenticationTestCase(TestCase):
     def test_is_authenticated(self):
@@ -22,23 +22,23 @@ class BasicAuthenticationTestCase(TestCase):
         request = HttpRequest()
         
         # No HTTP Basic auth details should fail.
-        self.assertEqual(auth.is_authenticated(request), False)
+        self.assertEqual(isinstance(auth.is_authenticated(request), HttpUnauthorized), True)
         
         # Wrong basic auth details.
         request.META['HTTP_AUTHORIZATION'] = 'abcdefg'
-        self.assertEqual(auth.is_authenticated(request), False)
+        self.assertEqual(isinstance(auth.is_authenticated(request), HttpUnauthorized), True)
         
         # No password.
         request.META['HTTP_AUTHORIZATION'] = base64.b64encode('daniel')
-        self.assertEqual(auth.is_authenticated(request), False)
+        self.assertEqual(isinstance(auth.is_authenticated(request), HttpUnauthorized), True)
         
         # Wrong user/password.
         request.META['HTTP_AUTHORIZATION'] = base64.b64encode('daniel:pass')
-        self.assertEqual(auth.is_authenticated(request), False)
+        self.assertEqual(isinstance(auth.is_authenticated(request), HttpUnauthorized), True)
         
         # Correct user/password.
         john_doe = User.objects.get(username='johndoe')
         john_doe.set_password('pass')
         john_doe.save()
-        request.META['HTTP_AUTHORIZATION'] = base64.b64encode('johndoe:pass')
+        request.META['HTTP_AUTHORIZATION'] = 'Basic %s' % base64.b64encode('johndoe:pass')
         self.assertEqual(auth.is_authenticated(request), True)
