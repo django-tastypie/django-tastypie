901eaeb8a196260df36d079fd895ea096eb33562
Daniel Lindsley <dlindsley@mediaphormedia.com>
2010-04-16 14:59:01 -0500

    Slightly more descriptive output when running all tests.

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 PYTHONPATH=`pwd`:`pwd`/..:$PYTHONPATH
+
+echo "** Core **"
 django-admin.py test core --settings=settings_core
+
+echo
+echo
+echo "** Basic **"
 django-admin.py test basic --settings=settings_basic
