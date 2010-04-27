182efb06f63916ac445fda827a60cd40f6e7ba9d
Cody Soyland <codysoyland@gmail.com>
2010-04-27 17:03:55 -0500

    Add HTTP POST test to basic HTTP test case.

--- tests/basic/tests/http.py
+++ tests/basic/tests/http.py
@@ -1,5 +1,6 @@
 from tests.testcases import TestServerTestCase
 import httplib
+import simplejson
 
 class HTTPTestCase(TestServerTestCase):
     def setUp(self):
@@ -36,3 +37,25 @@ class HTTPTestCase(TestServerTestCase):
         connection.close()
         self.assertEqual(response.status, 200)
         self.assertEqual(response.read(), '{"meta": {"limit": 20, "next": null, "offset": 0, "previous": null, "total_count": 2}, "objects": [{"content": "This is my very first post using my shiny new API. Pretty sweet, huh?", "created": "Tue, 30 Mar 2010 20:05:00 -0500", "is_active": true, "resource_uri": "/api/v1/notes/1/", "slug": "first-post", "title": "First Post!", "updated": "Tue, 30 Mar 2010 20:05:00 -0500", "user": "/api/v1/users/1/"}, {"content": "The dog ate my cat today. He looks seriously uncomfortable.", "created": "Wed, 31 Mar 2010 20:05:00 -0500", "is_active": true, "resource_uri": "/api/v1/notes/2/", "slug": "another-post", "title": "Another Post", "updated": "Wed, 31 Mar 2010 20:05:00 -0500", "user": "/api/v1/users/1/"}]}')
+
+    def test_post_object(self):
+        connection = self.get_connection()
+        post_data = '{"content": "A new post.", "is_active": true, "title": "New Title", "slug": "new-title", "user": "/api/v1/users/1/"}'
+        connection.request('POST', '/api/v1/notes/', body=post_data, headers={'Accept': 'application/json', 'Content-type': 'application/json'})
+        response = connection.getresponse()
+        self.assertEqual(response.status, 201)
+        self.assertEqual(dict(response.getheaders())['location'], 'http://localhost:8001/api/v1/notes/3/')
+
+        # make sure posted object exists
+        connection.request('GET', '/api/v1/notes/3/', headers={'Accept': 'application/json'})
+        response = connection.getresponse()
+        connection.close()
+
+        self.assertEqual(response.status, 200)
+
+        data = response.read()
+        obj = simplejson.loads(data)
+
+        self.assertEqual(obj['content'], 'A new post.')
+        self.assertEqual(obj['is_active'], True)
+        self.assertEqual(obj['user'], '/api/v1/users/1/')
