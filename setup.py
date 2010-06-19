ef668e3fda6d4cfd0ee906a05df37ecd69f7a47f
Daniel Lindsley <daniel@toastdriven.com>
2010-06-19 01:43:39 -0500

    Bumped version to v0.8.2.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='django-tastypie',
-    version='0.8.1',
+    version='0.8.2',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
