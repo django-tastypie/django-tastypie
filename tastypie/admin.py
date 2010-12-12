163db31992433976574391dec2be4ffa7cb9251b
Daniel Lindsley <daniel@toastdriven.com>
2010-12-12 00:47:53 -0600

    Fixed a missing import in ``admin.py``.

--- tastypie/admin.py
+++ tastypie/admin.py
@@ -1,9 +1,10 @@
+from django.conf import settings
 from django.contrib import admin
+
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     from tastypie.models import ApiKey
-
+    
     class ApiKeyInline(admin.StackedInline):
         model = ApiKey
         extra = 0
-
