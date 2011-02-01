4ff764ade02ee4f0ccb24afc6560637608db44c4
Milind <sh.milind@gmail.com>
2011-02-01 13:43:26 -0600

    removed oauth provider, depends on oauth provider plus https://bitbucket.org/david/django-oauth-plus

--- oauth_provider/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-
-from views import request_token, user_authorization, access_token
-
-urlpatterns = patterns('',
-    url(r'^request_token/$',    request_token,      name='oauth_request_token'),
-    url(r'^authorize/$',        user_authorization, name='oauth_user_authorization'),
-    url(r'^access_token/$',     access_token,       name='oauth_access_token'),
-)
