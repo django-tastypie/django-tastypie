1d7dd81f817e71bd8afcb632c2359462110b897b
Mikhail Korobov <kmike84@gmail.com>
2011-04-02 08:08:03 +0600

    Delete Content-Type header from HttpAccepted (204 No Content) response because this response can't have content. This header can be returned with HttpAccepted according to rfc2616, but it is useless (so removing it will slightly decrease bandwidth usage) and lint tools can complain about this header (e.g. Ian Bicking's wsgi lint middleware included in WebTest raises AssertionError for 204 responses with Content-Type header).

--- tastypie/http.py
+++ tastypie/http.py
@@ -6,18 +6,22 @@ from django.http import HttpResponse
 
 class HttpCreated(HttpResponse):
     status_code = 201
-    
+
     def __init__(self, *args, **kwargs):
         if 'location' in kwargs:
             location = kwargs['location']
             del(kwargs['location'])
-        
+
         super(HttpCreated, self).__init__(*args, **kwargs)
         self['Location'] = location
 
 
 class HttpAccepted(HttpResponse):
     status_code = 204
+
+    def __init__(self, *args, **kwargs):
+        super(HttpAccepted, self).__init__(*args, **kwargs)
+        del self['Content-Type']
 
 
 class HttpMultipleChoices(HttpResponse):
