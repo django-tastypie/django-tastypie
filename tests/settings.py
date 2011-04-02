591028998c5579ace104bc71ae3fdfb9c518aaf2
Mikhail Korobov <kmike84@gmail.com>
2011-04-02 15:15:02 +0600

    DeprecationWarnings during test running are fixed

--- tests/settings.py
+++ tests/settings.py
@@ -8,9 +8,19 @@ BASE_PATH = os.path.abspath(os.path.dirname(__file__))
 
 MEDIA_ROOT = os.path.normpath(os.path.join(BASE_PATH, 'media'))
 
+# for django < 1.2
 DATABASE_ENGINE = 'sqlite3'
 DATABASE_NAME = 'tastypie.db'
 TEST_DATABASE_NAME = 'tastypie-test.db'
+
+# for django >= 1.2
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'tastypie.db',
+        'TEST_NAME': 'tastypie-test.db',
+    }
+}
 
 INSTALLED_APPS = [
     'django.contrib.auth',
--- tests/slashless/tests.py
+++ tests/slashless/tests.py
@@ -10,23 +10,23 @@ except ImportError:
 
 class ViewsWithoutSlashesTestCase(TestCase):
     urls = 'slashless.api.urls'
-    
+
     def setUp(self):
         super(ViewsWithoutSlashesTestCase, self).setUp()
         self.old_debug = settings.DEBUG
         settings.DEBUG = True
-    
+
     def tearDown(self):
         settings.DEBUG = self.old_debug
         super(ViewsWithoutSlashesTestCase, self).tearDown()
-    
+
     def test_gets_without_trailing_slash(self):
         resp = self.client.get('/api/v1', data={'format': 'json'})
         self.assertEqual(resp.status_code, 200)
         deserialized = json.loads(resp.content)
         self.assertEqual(len(deserialized), 2)
         self.assertEqual(deserialized['notes'], {'list_endpoint': '/api/v1/notes', 'schema': '/api/v1/notes/schema'})
-        
+
         resp = self.client.get('/api/v1/notes', data={'format': 'json'})
         self.assertEqual(resp.status_code, 200)
         deserialized = json.loads(resp.content)
@@ -34,16 +34,17 @@ class ViewsWithoutSlashesTestCase(TestCase):
         self.assertEqual(deserialized['meta']['limit'], 20)
         self.assertEqual(len(deserialized['objects']), 2)
         self.assertEqual([obj['title'] for obj in deserialized['objects']], [u'First Post!', u'Another Post'])
-        
+
         resp = self.client.get('/api/v1/notes/1', data={'format': 'json'})
         self.assertEqual(resp.status_code, 200)
         deserialized = json.loads(resp.content)
         self.assertEqual(len(deserialized), 9)
         self.assertEqual(deserialized['title'], u'First Post!')
-        
+
         # Due to the way Django parses URLs, ``get_multiple`` won't work without
         # a trailing slash. This will cause the ``get_detail`` to match
         # instead, resulting in a 500.
         resp = self.client.get('/api/v1/notes/set/2;1', data={'format': 'json'})
         self.assertEqual(resp.status_code, 404)
         self.assertEqual(json.loads(resp.content)["error_message"], "Invalid resource lookup data provided (mismatched type).")
+
