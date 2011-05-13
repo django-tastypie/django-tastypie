3b240f15cf649db1361a11acf4ab4a2da541f6a5
James Hsiao <james.hsiao@cmgdigital.com>
2011-05-13 14:29:32 -0400

    Added ``DigestAuthentication``. Thanks to thepeopleseason for finishing the digest work I started & getting it tested/doc'd!

--- tests/core/tests/authentication.py
+++ tests/core/tests/authentication.py
@@ -1,9 +1,10 @@
 import base64
+import python_digest
 from django.contrib.auth.models import User
 from django.core import mail
 from django.http import HttpRequest
 from django.test import TestCase
-from tastypie.authentication import Authentication, BasicAuthentication, ApiKeyAuthentication
+from tastypie.authentication import Authentication, BasicAuthentication, ApiKeyAuthentication, DigestAuthentication
 from tastypie.http import HttpUnauthorized
 from tastypie.models import ApiKey
 
@@ -89,3 +90,49 @@ class ApiKeyAuthenticationTestCase(TestCase):
         request.GET['username'] = 'johndoe'
         request.GET['api_key'] = john_doe.api_key.key
         self.assertEqual(auth.is_authenticated(request), True)
+
+class DigestAuthenticationTestCase(TestCase):
+    fixtures = ['note_testdata.json']
+    
+    def test_is_authenticated(self):
+        auth = DigestAuthentication()
+        request = HttpRequest()
+        
+        # No HTTP Basic auth details should fail.
+        auth_request = auth.is_authenticated(request)
+        self.assertEqual(isinstance(auth_request, HttpUnauthorized), True)
+        
+        # HttpUnauthorized with auth type and realm
+        self.assertEqual(auth_request['WWW-Authenticate'].find('Digest'), 0)
+        self.assertEqual(auth_request['WWW-Authenticate'].find(' realm="django-tastypie"') > 0, True)
+        self.assertEqual(auth_request['WWW-Authenticate'].find(' opaque=') > 0, True)
+        self.assertEqual(auth_request['WWW-Authenticate'].find('nonce=') > 0, True)
+        
+        # Wrong basic auth details.
+        request.META['HTTP_AUTHORIZATION'] = 'abcdefg'
+        auth_request = auth.is_authenticated(request)
+        self.assertEqual(isinstance(auth_request, HttpUnauthorized), True)
+        
+        # No password.
+        request.META['HTTP_AUTHORIZATION'] = base64.b64encode('daniel')
+        auth_request = auth.is_authenticated(request)
+        self.assertEqual(isinstance(auth_request, HttpUnauthorized), True)
+        
+        # Wrong user/password.
+        request.META['HTTP_AUTHORIZATION'] = base64.b64encode('daniel:pass')
+        auth_request = auth.is_authenticated(request)
+        self.assertEqual(isinstance(auth_request, HttpUnauthorized), True)
+        
+        # Correct user/password.
+        john_doe = User.objects.get(username='johndoe')
+        john_doe.set_password('pass')
+        john_doe.save()
+        request.META['HTTP_AUTHORIZATION'] = python_digest.build_authorization_request(
+            john_doe.username,
+            request.method,
+            '/', # uri
+            1,   # nonce_count
+            digest_challenge=auth_request['WWW-Authenticate'],
+            password=john_doe.api_key.key)
+        auth_request = auth.is_authenticated(request)
+        self.assertEqual(auth_request, True)
