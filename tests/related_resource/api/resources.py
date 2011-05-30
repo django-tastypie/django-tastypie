53da08a351bba8f8a2c8db5301b54e3e8a614c5a
Marcel van den Elst <marcel@marutec.nl>
2011-05-30 19:00:26 +0200

    TestCase showing error introduced by commit 1988c7

--- tests/related_resource/api/resources.py
+++ tests/related_resource/api/resources.py
@@ -3,7 +3,7 @@ from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from core.models import Note
-from related_resource.models import Category
+from related_resource.models import Category, Tag, ExtraData, Taggable, TaggableTag
 
 
 class UserResource(ModelResource):
@@ -15,7 +15,7 @@ class UserResource(ModelResource):
 
 class NoteResource(ModelResource):
     author = fields.ForeignKey(UserResource, 'author')
-    
+
     class Meta:
         resource_name = 'notes'
         queryset = Note.objects.all()
@@ -24,9 +24,60 @@ class NoteResource(ModelResource):
 
 class CategoryResource(ModelResource):
     parent = fields.ToOneField('self', 'parent', null=True)
-    
+
     class Meta:
         resource_name = 'category'
         queryset = Category.objects.all()
+        authorization = Authorization()
+
+
+class TagResource(ModelResource):
+    taggabletags = fields.ToManyField(
+            'related_resource.api.resources.TaggableTagResource', 'taggabletags',
+            null=True)
+
+    extradata = fields.ToOneField(
+            'related_resource.api.resources.ExtraDataResource', 'extradata',
+            null=True, full=True)
+
+    class Meta:
+        resource_name = 'tag'
+        queryset = Tag.objects.all()
+        authorization = Authorization()
+
+
+class TaggableResource(ModelResource):
+    taggabletags = fields.ToManyField(
+            'related_resource.api.resources.TaggableTagResource', 'taggabletags',
+            null=True)
+
+    class Meta:
+        resource_name = 'taggable'
+        queryset = Taggable.objects.all()
+        authorization = Authorization()
+
+
+class TaggableTagResource(ModelResource):
+    tag = fields.ToOneField(
+            'related_resource.api.resources.TagResource', 'tag',
+            null=True)
+    taggable = fields.ToOneField(
+            'related_resource.api.resources.TaggableResource', 'taggable',
+            null=True)
+
+    class Meta:
+        resource_name = 'taggabletag'
+        queryset = TaggableTag.objects.all()
+        authorization = Authorization()
+
+
+class ExtraDataResource(ModelResource):
+    tag = fields.ToOneField(
+            'related_resource.api.resources.TagResource', 'tag',
+            null=True)
+
+    class Meta:
+        resource_name = 'extradata'
+        queryset = ExtraData.objects.all()
         authorization = Authorization()
 
