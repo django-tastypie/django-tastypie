4cc0b46931a231ff5f991912183066c9f23becb8
Mirat Can Bayrak <miratcanbayrak@gmail.com>
2011-07-01 09:45:58 +0300

    I found something missing at admin.py

--- tastypie/admin.py
+++ tastypie/admin.py
@@ -4,7 +4,16 @@ from django.contrib import admin
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     from tastypie.models import ApiKey
+    from django.contrib.auth.admin import UserAdmin
+    from django.contrib.auth.models import User
     
     class ApiKeyInline(admin.StackedInline):
         model = ApiKey
         extra = 0
+
+    class UserAdmin(UserAdmin):
+        inlines = [ApiKeyInline,]
+
+    admin.site.unregister(User)
+    admin.site.register(User, UserAdmin)
+
