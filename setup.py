3b443df71d265a38bfa99a9824d77f857889b0a2
Ed Summers <ehs@pobox.com>
2011-09-16 01:13:41 -0400

    forgot commands module

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     packages=[
         'tastypie',
         'tastypie.utils',
-        'tastypie.management',
+        'tastypie.management.commands',
     ],
     package_data={
         'tastypie': ['templates/tastypie/*'],
