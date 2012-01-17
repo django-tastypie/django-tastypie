498afa678bd6f1283c2d89badf1b298458cd0e2d
Daniel Lindsley <daniel@toastdriven.com>
2012-01-17 02:32:54 -0600

    Nevermind. That worked well on the ``User`` admin & blew everything else up.

--- tastypie/admin.py
+++ tastypie/admin.py
@@ -3,21 +3,11 @@ from django.contrib import admin
 
 
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
-    from django.contrib.auth.models import User
     from tastypie.models import ApiKey
 
     class ApiKeyInline(admin.StackedInline):
         model = ApiKey
         extra = 0
-
-    # Gross, gross, gross.
-    # Also, depends on the ordering of ``INSTALLED_APPS``.
-    m_admin = admin.site._registry[User]
-    m_admin.inlines.append(ApiKeyInline)
-
-    # Django 1.3 & below.
-    if hasattr(m_admin, 'inline_instances'):
-        m_admin.inline_instances.append(ApiKeyInline(User, admin.site))
 
     # Also.
     admin.site.register(ApiKey)
