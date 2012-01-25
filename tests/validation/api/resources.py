2a653f020ee4fe8c559977255fdef5cd787c520c
Vitaly Babiy <vbabiy86@gmail.com>
2012-01-25 12:45:23 -0500

    test case to show failing validation, when using nesting resources.

--- tests/validation/api/resources.py
+++ tests/validation/api/resources.py
@@ -3,7 +3,9 @@ from tastypie import fields
 from tastypie.constants import ALL
 from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
-from basic.models import Note
+from basic.models import Note, AnnotatedNote
+from django import forms
+from tastypie.validation import FormValidation
 
 
 class UserResource(ModelResource):
@@ -12,15 +14,34 @@ class UserResource(ModelResource):
         queryset = User.objects.all()
         authorization = Authorization()
 
+class AnnotatedNoteForm(forms.ModelForm):
+
+    class Meta:
+        model = AnnotatedNote
+
+class AnnotatedNoteResource(ModelResource):
+
+    class Meta:
+        resource_name = 'annotated'
+        queryset = AnnotatedNote.objects.all()
+        authorization = Authorization()
+        validation = FormValidation(form_class=AnnotatedNoteForm)
+
+class NoteForm(forms.ModelForm):
+
+    class Meta:
+        model = Note
 
 class NoteResource(ModelResource):
     user = fields.ForeignKey(UserResource, 'user')
-    
+    annotated_note = fields.ForeignKey(AnnotatedNote, 'annotated', null=True)
+
     class Meta:
         resource_name = 'notes'
         queryset = Note.objects.all()
         authorization = Authorization()
+        validation = FormValidation(form_class=NoteForm)
         filtering = {
             "created": ALL
             }
-        
+
--- tests/validation/tests.py
+++ tests/validation/tests.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.core.urlresolvers import reverse, NoReverseMatch
 from django.http import HttpRequest
 from django.test import TestCase
+import json
 
+settings.DEBUG = True
 try:
     import json
 except ImportError:
@@ -12,7 +14,7 @@ from basic.models import Note
 
 class FilteringErrorsTestCase(TestCase):
     urls = 'validation.api.urls'
-    
+
     def test_valid_date(self):
         resp = self.client.get('/api/v1/notes/', data={'format': 'json',
                                                       'created__gte':'2010-03-31'})
@@ -20,10 +22,28 @@ class FilteringErrorsTestCase(TestCase):
         deserialized = json.loads(resp.content)
         self.assertEqual(len(deserialized['objects']),
                          Note.objects.filter(created__gte='2010-03-31').count())
-        
+
 
     def test_invalid_date(self):
         resp = self.client.get('/api/v1/notes/', data={'format': 'json',
                                                       'created__gte':'foo-baz-bar'})
         self.assertEqual(resp.status_code, 400)
-        
+
+class NestResouceValidationTestCase(TestCase):
+    urls = 'validation.api.urls'
+
+    def test_valid_data(self):
+        data = json.dumps({
+            'title' : 'Test Title',
+            'slug' : 'test-title',
+            'content' : 'This is the content',
+            'user' : {'pk' : 1}, # loaded from fixtures
+            'annotated_note' : {'annotations' : 'This is an annotations'}
+        })
+
+        resp = self.client.post('/api/v1/notes/', data=data, content_type='application/json')
+        print json.loads(resp.content)['error_message']
+        print json.loads(resp.content)['traceback']
+        print resp.content
+        self.assertEqual(resp.status_code, 201)
+
