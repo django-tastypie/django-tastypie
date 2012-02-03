899dfaa8769e7bf4e1d73abb78bf52d4e98178b8
Glen Coates <glen@handshake-app.com>
2012-02-03 07:58:47 -0500

    Make ApiKey.key indexed in the DB for faster lookups

--- tastypie/models.py
+++ tastypie/models.py
@@ -18,10 +18,10 @@ class ApiAccess(models.Model):
     url = models.CharField(max_length=255, blank=True, default='')
     request_method = models.CharField(max_length=10, blank=True, default='')
     accessed = models.PositiveIntegerField()
-    
+
     def __unicode__(self):
         return u"%s @ %s" % (self.identifer, self.accessed)
-    
+
     def save(self, *args, **kwargs):
         self.accessed = int(time.time())
         return super(ApiAccess, self).save(*args, **kwargs)
@@ -31,28 +31,28 @@ if 'django.contrib.auth' in settings.INSTALLED_APPS:
     import uuid
     from django.conf import settings
     from django.contrib.auth.models import User
-    
+
     class ApiKey(models.Model):
         user = models.OneToOneField(User, related_name='api_key')
-        key = models.CharField(max_length=256, blank=True, default='')
+        key = models.CharField(max_length=256, blank=True, db_index=True, default='')
         created = models.DateTimeField(default=now)
 
         def __unicode__(self):
             return u"%s for %s" % (self.key, self.user)
-        
+
         def save(self, *args, **kwargs):
             if not self.key:
                 self.key = self.generate_key()
-            
+
             return super(ApiKey, self).save(*args, **kwargs)
-        
+
         def generate_key(self):
             # Get a random UUID.
             new_uuid = uuid.uuid4()
             # Hmac that beast.
             return hmac.new(str(new_uuid), digestmod=sha1).hexdigest()
-    
-    
+
+
     def create_api_key(sender, **kwargs):
         """
         A signal for hooking up automatic ``ApiKey`` creation.
