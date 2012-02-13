bdae31e7143270a03b6a53c3cf7184e3eccc1bc4
Josh Bohde <josh.bohde@gmail.com>
2012-02-12 20:17:17 -0600

    Catch the type error, and proceed to pk lookup.
    Unfortunately, this causes a Django "DoesNotExist" in "./../tastypie/fields.py", line 56

--- tastypie/fields.py
+++ tastypie/fields.py
@@ -546,7 +546,7 @@ class RelatedField(ApiField):
 
         try:
             return fk_resource.obj_update(fk_bundle, **data)
-        except NotFound:
+        except (NotFound, TypeError):
             try:
                 # Attempt lookup by primary key
                 lookup_kwargs = dict((k, v) for k, v in data.iteritems() if getattr(fk_resource, k).unique)
