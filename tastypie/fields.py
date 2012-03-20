df54d5addad48f151e88d3448442a91b8961db07
danger <danger@freebsd.org>
2012-03-20 17:34:19 +0100

    When RelatedField has full=True set, the ApiField.hydrate() method may return regular Bundle object (see line 168).
    In such case, the ApiField.build_related_resource() may obtain a regular Bundle as
    a value argument and it would fail with ApiFieldError even though there was a valid data.
    
    As such, teach ApiField.build_related_resource() about Bundle instances.

--- tastypie/fields.py
+++ tastypie/fields.py
@@ -585,6 +585,9 @@ class RelatedField(ApiField):
         if isinstance(value, basestring):
             # We got a URI. Load the object and assign it.
             return self.resource_from_uri(self.fk_resource, value, **kwargs)
+        elif isinstance(value, Bundle):
+            # We got a valid bundle object, the RelatedField had full=True
+            return value
         elif hasattr(value, 'items'):
             # We've got a data dictionary.
             # Since this leads to creation, this is the only one of these
