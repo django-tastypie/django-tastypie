4fb5ee9ac4bca7dbe973722102504377bc9ca698
Peter Baumgartner <pete@lincolnloop.com>
2012-03-23 15:34:43 -0600

    Fixes attribute error on patch_list
    Also adds tests for a couple untested PATCH scenarios:
    
    * Updating an existing resource
    * Fallback creation of a resource with a non-existent `resource_uri`

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -1293,7 +1293,6 @@ class Resource(object):
                     # so this is a create-by-PUT equivalent.
                     data = self.alter_deserialized_detail_data(request, data)
                     bundle = self.build_bundle(data=dict_strip_unicode_keys(data))
-                    bundle.obj.pk = obj.pk
                     self.obj_create(bundle, request=request)
             else:
                 # There's no resource URI, so this is a create call just
