8b99bf6903de721f3ebf3dd3ce2b4f766b3413f0
Simon Ye <sye737@gmail.com>
2012-04-25 02:23:11 -0700

    Adhere to ISO8601 strict. Millisecond precision only on datetime.

--- tastypie/serializers.py
+++ tastypie/serializers.py
@@ -124,7 +124,12 @@ class Serializer(object):
         if self.datetime_formatting == 'rfc-2822':
             return format_datetime(data)
 
-        return data.isoformat()
+        r = data.isoformat()
+        if data.microsecond:
+            r = r[:23] + r[26:]
+        if r.endswith('+00:00'):
+            r = r[:-6] + 'Z'
+        return r
 
     def format_date(self, data):
         """
