85ad5b7dfa77f60043943bc26470ae74a4867e28
Alec Koumjian <akoumjian@gmail.com>
2012-05-12 17:48:47 -0700

    Initial tests for new authorization hooks.

--- tests/authorization/urls.py
+++ tests/authorization/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls.defaults import patterns, url, include
 
 from tastypie.api import Api
-from .api.resources import ArticleResource
-v1_api = Api()
-v1_api.register(ArticleResource())
+from authorization.api.resources import UserResource, NoteResource
+v1_api = Api(api_name='v1')
+v1_api.register(UserResource())
+v1_api.register(NoteResource())
 
 
 urlpatterns = patterns('',
--- tests/settings_authorization.py
+++ tests/settings_authorization.py
@@ -1,6 +1,6 @@
 from settings import *
 
-INSTALLED_APPS.append('django.contrib.sites')
+INSTALLED_APPS.append('django.contrib.sessions')
 INSTALLED_APPS.append('authorization')
 
 ROOT_URLCONF = 'authorization.urls'
