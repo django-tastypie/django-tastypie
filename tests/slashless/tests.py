2d9d28f773e16f5ff83a1e79fbe7b69e39b84eb0
Philip Neustrom <philipn@gmail.com>
2012-09-08 23:12:11 -0700

    Slash-containing test for slashless.

--- tests/slashless/tests.py
+++ tests/slashless/tests.py
@@ -43,4 +43,30 @@ class ViewsWithoutSlashesTestCase(TestCase):
         deserialized = json.loads(resp.content)
         obj_ids = [o["id"] for o in deserialized["objects"]]
         self.assertEqual(sorted(obj_ids), [1,2])
+
+    def test_gets_wit_trailing_slash(self):
+        resp = self.client.get('/api/v1/', data={'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+        deserialized = json.loads(resp.content)
+        self.assertEqual(len(deserialized), 2)
+        self.assertEqual(deserialized['notes'], {'list_endpoint': '/api/v1/notes', 'schema': '/api/v1/notes/schema'})
         
+        resp = self.client.get('/api/v1/notes/', data={'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+        deserialized = json.loads(resp.content)
+        self.assertEqual(len(deserialized), 2)
+        self.assertEqual(deserialized['meta']['limit'], 20)
+        self.assertEqual(len(deserialized['objects']), 2)
+        self.assertEqual([obj['title'] for obj in deserialized['objects']], [u'First Post!', u'Another Post'])
+        
+        resp = self.client.get('/api/v1/notes/1/', data={'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+        deserialized = json.loads(resp.content)
+        self.assertEqual(len(deserialized), 9)
+        self.assertEqual(deserialized['title'], u'First Post!')
+        
+        resp = self.client.get('/api/v1/notes/set/2;1/', data={'format': 'json'})
+        self.assertEqual(resp.status_code, 200)
+        deserialized = json.loads(resp.content)
+        obj_ids = [o["id"] for o in deserialized["objects"]]
+        self.assertEqual(sorted(obj_ids), [1,2])
