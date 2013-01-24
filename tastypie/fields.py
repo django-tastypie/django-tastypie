c43b7da5cba79ff427c4fb7ac7dbe1886348e1a3
dbajal <satan.come.and.kill.your.family.and.u@gmail.com>
2013-01-24 18:24:33 +0400

    Update tastypie/fields.py
    If bundle.obj is not created when this check runs, django.db.fields.related ReverseSingleRelatedObjectDescriptor.__get__ will rise DoesNotExist error when we want our ApiFieldError.

--- tastypie/fields.py
+++ tastypie/fields.py
@@ -151,7 +151,7 @@ class ApiField(object):
                 return None
             elif self.attribute and getattr(bundle.obj, self.attribute, None):
                 return getattr(bundle.obj, self.attribute)
-            elif self.instance_name and hasattr(bundle.obj, self.instance_name):
+            elif self.instance_name and bundle.obj.pk and hasattr(bundle.obj, self.instance_name):
                 return getattr(bundle.obj, self.instance_name)
             elif self.has_default():
                 if callable(self._default):
