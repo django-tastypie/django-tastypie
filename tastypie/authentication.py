6dfc70f8bd0e4185da1aa8126776cac35102cf40
Lukas Klein <lukas@lukasklein.com>
2013-02-04 21:05:31 +0100

    Username field fix

--- tastypie/authentication.py
+++ tastypie/authentication.py
@@ -275,7 +275,7 @@ class SessionAuthentication(Authentication):
 
         This implementation returns the user's username.
         """
-        return request.user.username
+        return getattr(request.user, request.user.USERNAME_FIELD)
 
 
 class DigestAuthentication(Authentication):
