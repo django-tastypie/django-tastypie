609f2e29b3b0055dc1081f2fc138643e34716fb3
Daniel Lindsley <daniel@toastdriven.com>
2013-02-14 02:25:43 -0600

    Fixed a bug where JSON/JSONP was returning an invalid ``charset`` within the MIME type. Thanks to dstufft for the report!

--- tastypie/utils/mime.py
+++ tastypie/utils/mime.py
@@ -4,13 +4,13 @@ import mimeparse
 def determine_format(request, serializer, default_format='application/json'):
     """
     Tries to "smartly" determine which output format is desired.
-    
+
     First attempts to find a ``format`` override from the request and supplies
     that if found.
-    
+
     If no request format was demanded, it falls back to ``mimeparse`` and the
     ``Accepts`` header, allowing specification that way.
-    
+
     If still no format is found, returns the ``default_format`` (which defaults
     to ``application/json`` if not provided).
     """
@@ -18,11 +18,11 @@ def determine_format(request, serializer, default_format='application/json'):
     if request.GET.get('format'):
         if request.GET['format'] in serializer.formats:
             return serializer.get_mime_for_format(request.GET['format'])
-    
+
     # If callback parameter is present, use JSONP.
     if request.GET.has_key('callback'):
         return serializer.get_mime_for_format('jsonp')
-    
+
     # Try to fallback on the Accepts header.
     if request.META.get('HTTP_ACCEPT', '*/*') != '*/*':
         formats = list(serializer.supported_formats) or []
@@ -31,10 +31,10 @@ def determine_format(request, serializer, default_format='application/json'):
         # more information.
         formats.reverse()
         best_format = mimeparse.best_match(formats, request.META['HTTP_ACCEPT'])
-        
+
         if best_format:
             return best_format
-    
+
     # No valid 'Accept' header/formats. Sane default.
     return default_format
 
@@ -45,5 +45,8 @@ def build_content_type(format, encoding='utf-8'):
     """
     if 'charset' in format:
         return format
-    
+
+    if format in ('application/json', 'text/javascript'):
+        return format
+
     return "%s; charset=%s" % (format, encoding)
