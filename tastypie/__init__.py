fc02cca8268e4d6a4772f341ea5921f98a077d5b
Josh VanderLinden <joshua.vanderlinden@storagecraft.com>
2013-03-19 10:15:58 -0600

    Trying to fix the silly rose VERSION stuff

--- tastypie/__init__.py
+++ tastypie/__init__.py
@@ -2,8 +2,8 @@ import os
 import rose
 
 
-VERSION_FILE = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VERSION')
+VERSION_FILE = os.path.join(os.path.dirname(__file__), 'VERSION')
 
 
 __author__ = 'Daniel Lindsley, Cody Soyland, Matt Croydon, Josh Bohde & Issac Kelly'
-__version__ = rose.build_version('your_package_name_here', rose.load_version(VERSION_FILE))
+__version__ = rose.build_version('tastypie', rose.load_version(VERSION_FILE))
