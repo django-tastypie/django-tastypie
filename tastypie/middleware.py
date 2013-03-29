55d267453491acd62ef0ec696737ae13aeff9e3b
Vitaly Babiy <vbabiy86@gmail.com>
2013-03-28 23:09:48 -0400

    Tests for the TransactionMiddleware

--- tastypie/middleware.py
+++ tastypie/middleware.py
@@ -1,31 +1,15 @@
 from django.db import transaction
+from django.middleware.transaction import TransactionMiddleware
 
-class TransactionMiddleware(object):
+class TransactionMiddleware(TransactionMiddleware):
     """
-    Transaction middleware. If this is enabled, each view function will be run
-    with commit_on_response activated - that way a save() doesn't do a direct
-    commit, the commit is done when a successful response is created. If an
-    exception happens, the database is rolled back.
+    An extension of Djangos TransactionMiddleware, by also catching status codes
+    that are over 400. If a status code over 400 occurs it will roll back the transaction.
     """
-    def process_request(self, request):
-        """Enters transaction management"""
-        transaction.enter_transaction_management()
-        transaction.managed(True)
-
-    def process_exception(self, request, exception):
-        """Rolls back the database and leaves transaction management"""
-        if transaction.is_dirty():
-            transaction.rollback()
-        transaction.leave_transaction_management()
 
     def process_response(self, request, response):
         """Commits and leaves transaction management."""
-        if response.status_code in [400, 401, 403, 404, 405, 409, 410, 500, 501]:
+        if response.status_code >= 400:
             self.process_exception(request, None)
             return response
-
-        if transaction.is_managed():
-            if transaction.is_dirty():
-                transaction.commit()
-            transaction.leave_transaction_management()
-        return response
+        return super(TransactionMiddleware, self).process_response(request, response)
