8006eca018d73d9f716150488d7f70bd90e64533
Daniel Lindsley <daniel@toastdriven.com>
2013-05-02 19:40:40 -0700

    Started v0.9.16-dev.

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ except ImportError:
 
 setup(
     name='django-tastypie',
-    version='0.9.15',
+    version='0.9.16-dev',
     description='A flexible & capable API layer for Django.',
     author='Daniel Lindsley',
     author_email='daniel@toastdriven.com',
