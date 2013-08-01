a51142065e2cc6df0af0b13aff241ca0095a2ea8
Daniel Lindsley <daniel@toastdriven.com>
2013-07-31 23:18:27 -0700

    Core is passing all tests on Py3! \o/

--- tastypie/utils/validate_jsonp.py
+++ tastypie/utils/validate_jsonp.py
@@ -58,7 +58,7 @@ is_reserved_js_word = frozenset([
 # the core validation functions
 # ------------------------------------------------------------------------------
 
-def is_valid_javascript_identifier(identifier, escape=r'\u', ucd_cat=category):
+def is_valid_javascript_identifier(identifier, escape=r'\\u', ucd_cat=category):
     """Return whether the given ``id`` is a valid Javascript identifier."""
 
     if not identifier:
