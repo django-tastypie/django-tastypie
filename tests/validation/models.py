8f3f24ffe6f04853c45e68e6b9953f97212c6baf
Ian Wilson <ian.owings@gmail.com>
2013-08-25 21:43:32 -0400

    adding help text to validation.models.ValidatedModel

--- tests/validation/models.py
+++ tests/validation/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class ValidatedModel(models.Model):
+    """
+    Simple model that runs its own validation on save().
+    """
+
     content = models.CharField(max_length=20)
 
     def save(self, *args, **kwargs):
