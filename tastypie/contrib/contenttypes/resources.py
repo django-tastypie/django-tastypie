4a0f2d45763559b430b953f6147b736972d2e710
Ilya Beda <ir4y.ix@gmail.com>
2013-10-04 19:11:11 +0800

    Add gfk opbject creation as it

--- tastypie/contrib/contenttypes/resources.py
+++ tastypie/contrib/contenttypes/resources.py
@@ -1,8 +1,15 @@
 from __future__ import unicode_literals
-from tastypie.bundle import Bundle
+from tastypie import fields
 from tastypie.resources import ModelResource
 from tastypie.exceptions import NotFound
 from django.core.urlresolvers import resolve, Resolver404, get_script_prefix
+
+
+class ContentTypeResourceMixin(ModelResource):
+    content_type = fields.CharField()
+
+    def dehydrate_content_type(self, bundle):
+        return bundle.obj.__class__.__name__.lower()
 
 
 class GenericResource(ModelResource):
@@ -10,7 +17,11 @@ class GenericResource(ModelResource):
     Provides a stand-in resource for GFK relations.
     """
     def __init__(self, resources, *args, **kwargs):
-        self.resource_mapping = dict((r._meta.resource_name, r) for r in resources)
+        self.resources = resources
+        self.resource_mapping = dict((r._meta.resource_name, r)
+                                     for r in resources.values())
+        self.model_mapping = dict((v._meta.resource_name, k)
+                                  for k, v in resources.iteritems())
         return super(GenericResource, self).__init__(*args, **kwargs)
 
     def get_via_uri(self, uri, request=None):
@@ -32,11 +43,29 @@ class GenericResource(ModelResource):
         try:
             view, args, kwargs = resolve(chomped_uri)
             resource_name = kwargs['resource_name']
-            resource_class = self.resource_mapping[resource_name]
+            self.resource_mapping[resource_name]
         except (Resolver404, KeyError):
             raise NotFound("The URL provided '%s' was not a link to a valid resource." % uri)
 
-        parent_resource = resource_class(api_name=self._meta.api_name)
-        kwargs = parent_resource.remove_api_resource_names(kwargs)
-        bundle = Bundle(request=request)
-        return parent_resource.obj_get(bundle, **kwargs)
+    def get_resource_uri(self, bundle, *args, **kwargs):
+            Resource = self.resources[bundle.obj.__class__]
+            return Resource().get_resource_uri(bundle, *args, **kwargs)
+
+    def full_dehydrate(self, bundle, for_list=False):
+        Resource = self.resources[bundle.obj.__class__]
+
+        class _ProxyResource(ContentTypeResourceMixin, Resource):
+            def get_resource_uri(self, *args, **kwargs):
+                return Resource().get_resource_uri(*args, **kwargs)
+
+            class Meta(Resource.Meta):
+                pass
+        return _ProxyResource().full_dehydrate(bundle, for_list)
+
+    def full_hydrate(self, bundle):
+        Resource = self.resource_mapping[bundle.data['content_type']]
+        return Resource().full_hydrate(bundle)
+
+    def save(self, bundle, skip_errors=False):
+        Resource = self.resource_mapping[bundle.data['content_type']]
+        return Resource().save(bundle, skip_errors)
