9efaef961d5c07db85ab3882a428407eedf1c285
Val Neekman <val@neekware.com>
2014-03-09 22:30:51 -0400

    lazy load User model

--- tastypie/compat.py
+++ tastypie/compat.py
@@ -2,17 +2,18 @@ from __future__ import unicode_literals
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 import django
+from django.utils.functional import lazy
 
 __all__ = ['User', 'AUTH_USER_MODEL']
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 # Django 1.5+ compatibility
 if django.VERSION >= (1, 5):
+    AUTH_USER_MODEL = settings.AUTH_USER_MODEL
     try:
         from django.contrib.auth import get_user_model
-        User = get_user_model()
-        username_field = User.USERNAME_FIELD
+        User = lazy(get_user_model, AUTH_USER_MODEL)
+        username_field = lazy(lambda: User.USERNAME_FIELD, str)
     except ImproperlyConfigured:
         # The the users model might not be read yet.
         # This can happen is when setting up the create_api_key signal, in your
@@ -21,4 +22,5 @@ if django.VERSION >= (1, 5):
         username_field = None
 else:
     from django.contrib.auth.models import User
+    AUTH_USER_MODEL = 'auth.User'
     username_field = 'username'
