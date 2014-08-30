68baf4c9708c592e1069de983f19098905bf69a3
Ilya Baryshev <baryshev@gmail.com>
2014-08-30 19:58:52 +0400

    Django 1.7: Lazy loading of User model in management command
    After app loading change, User model can no longer be referenced
    while app registry is not populated.
    
    This commit completes work in c7aa6c8

--- tastypie/management/commands/backfill_api_keys.py
+++ tastypie/management/commands/backfill_api_keys.py
@@ -1,7 +1,7 @@
 from __future__ import print_function
 from __future__ import unicode_literals
 from django.core.management.base import NoArgsCommand
-from tastypie.compat import User
+from tastypie.compat import get_user_model
 from tastypie.models import ApiKey
 
 
@@ -12,6 +12,7 @@ class Command(NoArgsCommand):
         """Goes through all users and adds API keys for any that don't have one."""
         self.verbosity = int(options.get('verbosity', 1))
 
+        User = get_user_model()
         for user in User.objects.all().iterator():
             try:
                 api_key = ApiKey.objects.get(user=user)
