d38133435cf3d3363f5669120a199089a2d105ad
Ash Christopher <ash.christopher@gmail.com>
2014-09-12 13:29:43 -0400

    Replaced additional imports

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -3,7 +3,12 @@ import logging
 import warnings
 import django
 from django.conf import settings
-from django.conf.urls.defaults import patterns, url
+
+try:
+    from django.conf.urls import patterns, url, include
+except ImportError: # Django < 1.4
+    from django.conf.urls.defaults import patterns, url, include
+
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
 from django.core.urlresolvers import NoReverseMatch, reverse, resolve, Resolver404, get_script_prefix
 from django.db import transaction
