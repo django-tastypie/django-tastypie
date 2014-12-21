f452a63f51ac0ed5ce949c9eb2bebd9d76e72fc6
Nick Sullivan <nick@sullivanflock.com>
2014-12-21 14:04:52 -0800

    Improved admin UI for API Keys (Closes #1262)
    * Make api keys searchable, by key or username
    * Displays more information on the initial screen, with sortable columns
    * Use raw_id_fields for user, because detail page can get very slow when
        there are a lot of api keys

--- docs/release_notes/dev.rst
+++ docs/release_notes/dev.rst
@@ -12,3 +12,4 @@ Bugfixes
 * Fix get_via_uri not working for alphabetic ids that contain the resource name (Fixes #1239, Closes #1240)
 * Don't enable unsupported formats by default. (Fixes #1451)
 * Gave ApiKey a __str__ implementation that works in Python 2 and 3. (Fixes #1459, Closes #1460)
+* Improved admin UI for API Keys (Closes #1262)
--- tastypie/admin.py
+++ tastypie/admin.py
@@ -6,9 +6,16 @@ from django.contrib import admin
 if 'django.contrib.auth' in settings.INSTALLED_APPS:
     from tastypie.models import ApiKey
 
+
     class ApiKeyInline(admin.StackedInline):
         model = ApiKey
         extra = 0
+
+
+    class ApiKeyAdmin(admin.ModelAdmin):
+        search_fields = ['key', 'user__username']
+        raw_id_fields = ['user']
+        list_display = ['id', 'user', 'key', 'created']
 
     ABSTRACT_APIKEY = getattr(settings, 'TASTYPIE_ABSTRACT_APIKEY', False)
 
@@ -17,4 +24,4 @@ if 'django.contrib.auth' in settings.INSTALLED_APPS:
                         "or 'False'.")
 
     if not ABSTRACT_APIKEY:
-        admin.site.register(ApiKey)
+        admin.site.register(ApiKey, ApiKeyAdmin)
