d5cc1f469ad2cd38babef590c525669644a2d22a
Willem Bult <willem.bult@gmail.com>
2014-12-22 18:14:44 -0800

    Django 1.8 compatibility

--- tastypie/compat.py
+++ tastypie/compat.py
@@ -23,3 +23,9 @@ else:
 
     def get_username_field():
         return 'username'
+
+def get_module_name(meta):
+    return getattr(meta, 'model_name', None) or getattr(meta, 'module_name')
+
+# commit_on_success replaced by atomic in Django >=1.8
+atomic_decorator = getattr(django.db.transaction, 'atomic', None) or getattr(django.db.transaction, 'commit_on_success')
