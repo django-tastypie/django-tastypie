db9c2c03c0422088d1ad11313c8610c530d0dc4b
Phoebe B <phoebebright310@gmail.com>
2015-09-10 09:23:18 +0100

    If GEOS is not installed then exclude geos related calls and don't run tests for gis and gis_spatialite

--- tastypie/resources.py
+++ tastypie/resources.py
@@ -10,7 +10,14 @@ from wsgiref.handlers import format_date_time
 import django
 from django.conf import settings
 from django.conf.urls import patterns, url
-from django.contrib.gis.db.models.fields import GeometryField
+
+# Django 1.8 will run without GEOS so fail silently if not installed
+try:
+    from django.contrib.gis.db.models.fields import GeometryField
+    use_geos = True
+except:
+    use_geos = False
+
 from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned, ValidationError
 from django.core.urlresolvers import NoReverseMatch, reverse, resolve, Resolver404, get_script_prefix
 from django.core.signals import got_request_exception
@@ -1972,7 +1979,7 @@ class BaseModelResource(Resource):
             query_terms = self._meta.queryset.query.query_terms
         else:
             query_terms = QUERY_TERMS
-        if django.VERSION >= (1, 8):
+        if django.VERSION >= (1, 8) and use_geos:
             query_terms = query_terms | set(GeometryField.class_lookups.keys())
 
         for filter_expr, value in filters.items():
--- tests/manage_gis_spatialite.py
+++ tests/manage_gis_spatialite.py
@@ -2,8 +2,14 @@
 import os, sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_gis_spatialite")
 
-    from django.core.management import execute_from_command_line
+    # Django 1.8 will run without GEOS so don't run tests if not installed
+    try:
 
-    execute_from_command_line(sys.argv)
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings_gis_spatialite")
+
+        from django.core.management import execute_from_command_line
+
+        execute_from_command_line(sys.argv)
+    except:
+        print "GEOS Not installed - not testing gis_spatialite"
