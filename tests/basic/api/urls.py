a59ea9b93c89be6f22ebeddeaade9cc25d4881a4
Martin Guerrero <martin.rodriguezg@interalia.net>
2015-09-16 13:43:48 -0500

    add test for return right message when datefield. Issue #1268

--- tests/basic/api/urls.py
+++ tests/basic/api/urls.py
@@ -4,6 +4,7 @@ except ImportError: # Django < 1.4
     from django.conf.urls.defaults import *
 from tastypie.api import Api
 from basic.api.resources import NoteResource, UserResource, BustedResource, CachedUserResource, PublicCachedUserResource, PrivateCachedUserResource, SlugBasedNoteResource, SessionUserResource
+from customuser.api.resources import CustomUserResource
 
 api = Api(api_name='v1')
 api.register(NoteResource(), canonical=True)
@@ -16,5 +17,6 @@ v2_api = Api(api_name='v2')
 v2_api.register(BustedResource(), canonical=True)
 v2_api.register(SlugBasedNoteResource())
 v2_api.register(SessionUserResource())
+v2_api.register(CustomUserResource())
 
 urlpatterns = v2_api.urls + api.urls
