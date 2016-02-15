779fe7dede08a6a18a24c1f67a1638c614d1e0a3
Sean Hayes <sean@seanhayes.name>
2016-02-14 20:55:01 -0800

    Updating for v0.13.2 release.

--- tastypie/__init__.py
+++ tastypie/__init__.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 
 __author__ = 'Daniel Lindsley & the Tastypie core team'
 
-VERSION = (0, 13, 1)
+VERSION = (0, 13, 2)
 
 __short_version__ = '.'.join(map(str, VERSION[0:2]))
 __version__ = ''.join(['.'.join(map(str, VERSION[0:3])), ''.join(VERSION[3:])])
