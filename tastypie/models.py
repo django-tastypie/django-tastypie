bb30ee5aa7d13869fef9d6db1830ff3848306ea5
Matt Cooper <vtbassmatt@gmail.com>
2016-03-20 07:44:22 -0700

    fix ApiKey __str__ under Py3

--- tastypie/models.py
+++ tastypie/models.py
@@ -31,12 +31,13 @@ if 'django.contrib.auth' in settings.INSTALLED_APPS:
     import uuid
     from tastypie.compat import AUTH_USER_MODEL
 
+    @python_2_unicode_compatible
     class ApiKey(models.Model):
         user = models.OneToOneField(AUTH_USER_MODEL, related_name='api_key')
         key = models.CharField(max_length=128, blank=True, default='', db_index=True)
         created = models.DateTimeField(default=now)
 
-        def __unicode__(self):
+        def __str__(self):
             return u"%s for %s" % (self.key, self.user)
 
         def save(self, *args, **kwargs):
