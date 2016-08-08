django-tastypie/django-tastypie

commit af2c1d667f97a1a576e336766d7ac6245263b39d

Avoid modifying Field instances during request/response cycle. (closes #1415)

tastypie/fields.py
@@ -744,6 +744,19 @@ class ToOneField(RelatedField):
             full_detail=full_detail
         )
 
+    def contribute_to_class(self, cls, name):
+        super(ToOneField, self).contribute_to_class(cls, name)
+        if not self.related_name:
+            related_field = getattr(self._resource._meta.object_class, self.attribute, None)
+            if isinstance(related_field, ReverseOneToOneDescriptor):
+                # This is the case when we are writing to a reverse one to one field.
+                # Enable related name to make this work fantastically.
+                # see https://code.djangoproject.com/ticket/18638 (bug; closed; worksforme)
+                # and https://github.com/django-tastypie/django-tastypie/issues/566
+
+                # this gets the related_name of the one to one field of our model
+                self.related_name = related_field.related.field.name
+
     def dehydrate(self, bundle, for_list=True):
         foreign_obj = None
 
@@ -777,15 +790,6 @@ class ToOneField(RelatedField):
 
         if value is None:
             return value
-
-        if bundle.obj and isinstance(getattr(bundle.obj.__class__, self.attribute), ReverseOneToOneDescriptor):
-            # This is the case when we are writing to a reverse one to one field.
-            # Enable related name to make this work fantastically.
-            # see https://code.djangoproject.com/ticket/18638 (bug; closed; worksforme)
-            # and https://github.com/django-tastypie/django-tastypie/issues/566
-
-            # this gets the related_name of the one to one field of our model
-            self.related_name = getattr(bundle.obj.__class__, self.attribute).related.field.name
 
         return self.build_related_resource(value, request=bundle.request)
 

commit b5d7c860e4d6ef4641c7185c70549dea5fca5140

Fixed bug in ToOneFields when attribute is not a string
This addresses a regression introduced in af2c1d667f97a1a576e336766d7ac6245263b39d where a ToOneField with self.attribute set to None or a function would raise an exception during resource initialization.

tastypie/fields.py
@@ -741,7 +741,7 @@ class ToOneField(RelatedField):
 
     def contribute_to_class(self, cls, name):
         super(ToOneField, self).contribute_to_class(cls, name)
-        if not self.related_name:
+        if not self.related_name and isinstance(self.attribute, six.string_types):
             related_field = getattr(self._resource._meta.object_class, self.attribute, None)
             if isinstance(related_field, ReverseOneToOneDescriptor):
                 # This is the case when we are writing to a reverse one to one field.

tests/core/tests/resources.py
@@ -1120,6 +1120,24 @@ class RequiredFKNoteResource(ModelResource):
         authorization = Authorization()
 
 
+class NoAttributeFKNoteResource(ModelResource):
+    editor = fields.ForeignKey(UserResource, None)
+
+    class Meta:
+        resource_name = 'noattrfknotes'
+        queryset = NoteWithEditor.objects.all()
+        authorization = Authorization()
+
+
+class FunctionAttributeFKNoteResource(ModelResource):
+    editor = fields.ForeignKey(UserResource, lambda bundle: User.objects.first(), null=True)
+
+    class Meta:
+        resource_name = 'fnattrfknotes'
+        queryset = NoteWithEditor.objects.all()
+        authorization = Authorization()
+
+
 class ThrottledNoteResource(NoteResource):
     class Meta:
         resource_name = 'throttlednotes'
@@ -1488,6 +1506,12 @@ class ModelResourceTestCase(TestCase):
 
         resource_6 = CustomPageNoteResource()
         self.assertEqual(resource_6._meta.paginator_class, CustomPaginator)
+
+        # FK fields with non-string attributes
+        resource_7 = NoAttributeFKNoteResource()
+        self.assertEqual(len(resource_7.fields), 9)
+        resource_8 = FunctionAttributeFKNoteResource()
+        self.assertEqual(len(resource_8.fields), 9)
 
     def test_can_create(self):
         resource_1 = NoteResource()