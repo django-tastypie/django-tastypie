67e50d6ead32b958e214c8ce0a854eb848a68949
George Dorn <gdorn_gh@circuitlocution.com>
2024-04-23 13:25:21 -0700

    v0.14.7 bump (django 5.0)

--- tastypie/__init__.py
+++ tastypie/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Daniel Lindsley & the Tastypie core team'
 
-VERSION = (0, 14, 6)
+VERSION = (0, 14, 7)
 
 __short_version__ = '.'.join(map(str, VERSION[0:2]))
 __version__ = ''.join(['.'.join(map(str, VERSION[0:3])), ''.join(VERSION[3:])])
