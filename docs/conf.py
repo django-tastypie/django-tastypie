cf7379ad2c5498d237830026d8c5234b60a89345
Sam Thompson <georgedorn@users.noreply.github.com>
2024-10-15 13:21:56 -0700

    Pin Sphinx to last known working version for now (#1682)
    * Pin Sphinx to last known working version for now
    
    * Update Sphinx and move its requirements into a proper requirements.txt
    
    * Stop running tests and docs for EOL versions of Django/Python

--- docs/conf.py
+++ docs/conf.py
@@ -108,12 +108,11 @@ pygments_style = 'sphinx'
 html_theme = 'default'
 
 try:
-    import sphinx_rtd_theme
+    import sphinx_rtd_theme  # noqa
 except ImportError:
     pass
 else:
     html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
