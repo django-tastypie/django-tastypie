bd07c587c86c03592dde35910585f2212719fc6d
George Dorn <gdorn_gh@circuitlocution.com>
2025-02-23 11:17:44 -0800

    Version bump for release

--- tastypie/__init__.py
+++ tastypie/__init__.py
@@ -1,6 +1,6 @@
 __author__ = 'Daniel Lindsley & the Tastypie core team'
 
-VERSION = (0, 15, 0)
+VERSION = (0, 15, 1)
 
 __short_version__ = '.'.join(map(str, VERSION[0:2]))
 __version__ = ''.join(['.'.join(map(str, VERSION[0:3])), ''.join(VERSION[3:])])
